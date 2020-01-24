9110b74585c90c7b2b8da04e80d105a03ba1e14b
keshon <keshon@zoho.com>
2020-01-24 16:48:39 +0300

    Update start-restart--nix.sh

--- start-restart--nix.sh
+++ start-restart--nix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DIR_NAME="lib"                           # if empty - root directory of docker-compose file is used by default
-export LIBRARY="${DIR_NAME}/ExampleLibrary.php" # if empty - "tests/test-libraries/ExampleLibrary.php" is used by default
-export PORT=11996                               # if empty - 8111 port is used by default
+export DIR_NAME="lib"
+export LIBRARY="${DIR_NAME}/ExampleLibrary.php"
+export PORT=11996
 
 php src/StartRobotRemoteServer.php $LIBRARY $PORT
